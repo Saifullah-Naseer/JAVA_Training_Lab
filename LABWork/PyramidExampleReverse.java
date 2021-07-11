7463d46d662f9bd2ad63e895ba6bdc2be332425c
Saifullah-Naseer <87148631+Saifullah-Naseer@users.noreply.github.com>
2021-07-11 18:56:12 +0530

    Add files via upload

---
+++ LABWork/PyramidExampleReverse.java
@@ -0,0 +1,13 @@
+package LABWork;
+
+public class PyramidExampleReverse {
+public static void main(String[] args) {
+	         int term=6;
+	         for(int i=1;i<=term;i++)
+	         {
+	        	 System.out.print("* ");
+	         }	 
+	         System.out.println();//new line
+         }
+     }
+
